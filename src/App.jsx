552b6fad8c7240bda339f97d8449d97bcce2c0a2
git_VictorPrinze <kariukivictor226@gmail.com>
2024-04-27 19:16:27 +0300

    added major weather app functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Page from "./Page";
+import {
+    FavouriteProvider,
+    LocationProvider,
+    WeatherProvider,
+} from "./provider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <LocationProvider>
+            <WeatherProvider>
+                <FavouriteProvider>
+                    <Page />
+                </FavouriteProvider>
+            </WeatherProvider>
+        </LocationProvider>
+    );
 }
 
 export default App;
